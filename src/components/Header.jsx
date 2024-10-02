d2be906cb57d5489dd22619617304853bdb2b70d
r0hhhan <rohanpatil3343@gmail.com>
2024-10-03 00:07:42 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='flex items-center justify-between gap-4 p-4'>
+            <a href="/"><h1 className='font-medium'>Free<span className='text-blue-400 bold'>Scribe</span></h1></a>
+        </header>
+    )
+}
