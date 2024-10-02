c620c43c92c547bae6b0ab417764a92704af3ac4
r0hhhan <rohanpatil3343@gmail.com>
2024-10-03 00:25:59 +0530

    added linkedin url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className='flex items-center justify-between gap-4 p-4'>
             <a href="/"><h1 className='font-medium'>Speak<span className='text-blue-400 bold'>Shift</span></h1></a>
+            <a href="https://www.linkedin.com/in/rohanpatil3343/"><h1 className='font-medium'>Reach me</h1></a>
         </header>
     )
 }
